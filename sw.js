af58c96f516b68a6ef797754d0435bbdd525c56b
INSOMNIAradio <SSCORPIASS@GMAIL.COM>
2026-07-14 23:54:49 +0200

    Add APK download

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "insomnia-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/INSOMNIA_perfil.png",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
